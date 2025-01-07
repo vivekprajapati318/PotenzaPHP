2ef2b57feb32e6b5222559f2365465ad4d256ede
vivek <vivekprajapati318@gmail.com>
2025-01-07 18:01:31 +0530

    done

--- StripePayment/SMSOTP/varify.php
+++ StripePayment/SMSOTP/varify.php
@@ -9,9 +9,9 @@ $to = $_POST['number'];
 $message = rand(111111, 999999);
 try {
 
-    $account_sid = 'YOUR AcCOUNT ID';
-    $auth_token = 'YOUR AUTH TOKEn';
-    $twilio_phone_number = 'NUMBER';
+    // $account_sid = 'YOUR AcCOUNT ID';
+    // $auth_token = 'YOUR AUTH TOKEn';
+    // $twilio_phone_number = 'NUMBER';
     $client = new Client($account_sid, $auth_token);
     $messageResponse = $client->messages->create(
         $to,
